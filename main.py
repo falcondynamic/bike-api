62eea14c7d5a9b28c861d99f752b12ea903f6aa8
Mustafa Akbas <gyrfalcondynamics@gmail.com>
2024-06-11 19:42:04 +0100

    add endpoint prediction

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
+
+@app.get('/prediction/{param1}/{param2}/{param3}/{param4}/{param5}/{param6}"')
+def prediction(param1: int, param2: int, param3: str, param4: int, param5: str, param6: str):
+    return {"message": f"Prediction! {param1}, {param2}, {param3}, {param4}, {param5}, {param6}"}
