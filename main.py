6cd198a0f10d0cfb5d3bc8a4be198a3eede775d4
Mustafa Akbas <gyrfalcondynamics@gmail.com>
2024-06-11 20:14:32 +0100

    prediction model

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
+import joblib
+import pandas as pd
 
 app = FastAPI()
+
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
 
 @app.get('/prediction/{param1}/{param2}/{param3}/{param4}/{param5}/{param6}')
-def prediction(param1: int, param2: int, param3: str, param4: int, param5: str, param6: str):
-    return {"message": f"Prediction! {param1}, {param2}, {param3}, {param4}, {param5}, {param6}"}
+async def prediction(param1: int, param2: int, param3: str, param4: int, param5: str, param6: str):
+
+    my_dict = {
+        "gabel_federweg_mm": param1,
+        "akkukapazität_wh": param2,
+        "rahmenmaterial": param3,
+        'gänge': param4,
+        "kategorie": param5,
+        "hersteller": param6    
+    }
+
+    df = pd.DataFrame.from_dict([my_dict])
+
+    prediction = rf_model.predict(df)[0]
+
+    return {"message": f"Prediction! {param1}, {param2}, {param3}, {param4}, {param5}, {param6} Result: {prediction}"}
