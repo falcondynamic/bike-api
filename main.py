68b642a2004037f97b9434bf523dafe5d52f820d
Mustafa Akbas <gyrfalcondynamics@gmail.com>
2024-06-11 19:57:47 +0100

    fix

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello World!"}
 
-@app.get('/prediction/{param1}/{param2}/{param3}/{param4}/{param5}/{param6}"')
+@app.get('/prediction/{param1}/{param2}/{param3}/{param4}/{param5}/{param6}')
 def prediction(param1: int, param2: int, param3: str, param4: int, param5: str, param6: str):
     return {"message": f"Prediction! {param1}, {param2}, {param3}, {param4}, {param5}, {param6}"}
